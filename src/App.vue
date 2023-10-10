3f14aecc00f82186eac18e160b4d6671f1145928
julyueluo <julyueluo@163.com>
2023-10-11 01:10:02 +0800

    rr

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@ import TheWelcome from './components/TheWelcome.vue'
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/Screenshot_3.bmp" width="125" height="125" />
+    <img alt="Vue logo" class="logo" src="./assets/qq.jpg" width="125" height="125" />
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
