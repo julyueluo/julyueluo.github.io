fdf63d5f17729fb50c7123457e31c46aec9bb21c
julyueluo <julyueluo@163.com>
2023-10-11 14:38:40 +0800

    later

--- src/App.vue
+++ src/App.vue
@@ -4,14 +4,27 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/svg/404.svg" width="125" height="125" />
+    <!-- <img alt="Vue logo" class="logo" src="./assets/svg/education.svg" width="125" height="125" /> -->
+    <img alt="Vue logo" class="logo" src="./assets/master-albrecht-voss-homepage-banner.jpg" width="125" height="125" />
+
     <div class="wrapper">
       <HelloWorld msg="你好" />
     </div>
+
   </header>
 
   <main>
-
+    <div>
+      <img alt="Vue logo" class="logo" src="./assets/master-albrecht-voss-homepage-banner2.jpg" width="125"
+        height="125" />
+      <h2>长歌行</h2>
+      <p>汉乐府〔两汉〕</p>
+      <p>青青园中葵，朝露待日晞。</p>
+      <p>阳春布德泽，万物生光辉。</p>
+      <p>常恐秋节至，焜黄华叶衰。</p>
+      <p>百川东到海，何时复西归？</p>
+      <p>少壮不努力，老大徒伤悲！</p>
+    </div>
   </main>
 </template>
 
