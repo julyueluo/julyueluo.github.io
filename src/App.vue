504c858b52db82b1eb1a6d042b68da3ad59d8178
julyueluo <julyueluo@163.com>
2023-10-11 11:24:17 +0800

    dd

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/qq.jpg" width="125" height="125" />
+    <img alt="Vue logo" class="logo" src="./assets/svg/404.svg" width="125" height="125" />
     <div class="wrapper">
       <HelloWorld msg="你好" />
     </div>
